4295a92a1221f5a1a77a20bfe9f9f22043a914c5
Nhan <nhan4@msn.com>
2022-03-30 19:38:56 -0700

    :sparkles: initial implementation of CArouselButton

--- src/CarouselButton.js
+++ src/CarouselButton.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const CarouselButton = () => <button />;
+const CarouselButton = (props) => <button {...props} />; //1
+
+CarouselButton.propTypes = {
+  children: PropTypes.node.isRequred,
+};
 
 export default CarouselButton;
